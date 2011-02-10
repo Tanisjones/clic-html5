36788cea4d9cece11faf7b219ad8fc239715c3d7
Jordi Duch <jordi.duch@gmail.com>
2011-02-10 17:44:59 +0100

    Changes the game to dragp and drop boxes

--- js/game.js
+++ js/game.js
@@ -1,58 +1,64 @@
+var imagelist = [
+     { "src" : './images/grass.jpg', "posx" : 200, "posy" : 200, "w" : 128, "h" : 128},
+     { "src" : './images/roof.jpg', "posx" : 300, "posy" : 300, "w" : 128, "h" : 128},
+     { "src" : './images/brick.png', "posx" : 400, "posy" : 400, "w" : 128, "h" : 128}
+];
 
-$(function () {
-  var canvas = $("#canvas");
-  canvasWidth  = canvas.width();
-  canvasHeight = canvas.height();
 
-  var context = canvas[0].getContext("2d");
+////////////////////////////////////////////////
+var canvas = $("#canvas");
+canvasWidth  = canvas.width();
+canvasHeight = canvas.height();
 
-  Text.context = context;
-  Text.face = vector_battle;
+var context = canvas[0].getContext("2d");
 
-  var i, j = 0;
-  var avgFramerate = 0;
-  var frameCount = 0;
-  var elapsedCounter = 0;
+Text.context = context;
+Text.face = vector_battle;
 
-  var lastFrame = Date.now();
-  var thisFrame;
-  var elapsed;
-  var delta;
-
+var myimages = new ImageSet();
   
-  // MAIN LOOP FUNCTION
-  var mainLoop = function () {
+//LOAD THE IMAGES FROM THE DATA
+var i;
+for (i=0;i<imagelist.length;i++){
+	var img = new ImageData(i,context,imagelist[i].src);
+	img.setPosition(imagelist[i].posx,imagelist[i].posy);
+	img.setSize(imagelist[i].w,imagelist[i].h);
+	myimages.add(img);
+}
+
+var frontImage='none';
+  
+// MAIN LOOP FUNCTION
+var mainLoop = function () {
 	  
 	//CLEAR SCREEN
-    context.clearRect(0, 0, canvasWidth, canvasHeight);
+	context.clearRect(0, 0, canvasWidth, canvasHeight);
 
-    //COUNT THE TIME AND FRAMES PER SECOND
-    thisFrame = Date.now();
-    elapsed = thisFrame - lastFrame;
-    lastFrame = thisFrame;
-    frameCount++;
-    elapsedCounter += elapsed;
-    if (elapsedCounter > 1000) {
-      elapsedCounter -= 1000;
-      avgFramerate = frameCount;
-      frameCount = 0;
-    }
-    delta = elapsed / 30;
-    
-    // PRINT THE FRAME RATE
-    Text.renderText('FPS:'+avgFramerate, 24, canvasWidth - 118, canvasHeight - 2);
+	//DRAW THE IMAGE
+	myimages.draw();
+	
+	if(DragData.active)
+	{
+		Text.renderText('DRAG FROM: '+DragData.startPosX+' '+DragData.startPosY, 24, 10,30);  
+		
+		//Choose the selected image and activate it
+		if(frontImage=='none'){
+			frontImage=myimages.getFrontImage(DragData.startPosX, DragData.startPosY);
+			if(frontImage!='notfound') frontImage.setDraggable();
+		}
+		//Move the dragged image around
+		if(frontImage!='notfound'){
+			Text.renderText('IMAGE: '+frontImage.id, 24, 10,60);  
+			frontImage.drag(DragData.startPosX-DragData.currentPosX, DragData.startPosY-DragData.currentPosY);
+		}
+		
+	//Disable the current active image
+	}else if(frontImage!='none'){
+		if(frontImage!='notfound') frontImage.unsetDraggable();
+		frontImage='none';
+	}
+};
 
-    if (TouchData.numFingers>0) 
-    {
-    	for(i=0; i<TouchData.numFingers; i++)
-    	{
-        	Text.renderText('FINGER:'+TouchData.posX[0]+' '+TouchData.posY[0], 24, 10,30+30*i);    	
-    	}
-    	TouchData.numFingers=0;
-    }
-    
-  };
+// START MAINLOOP
+var mainLoopId = setInterval(mainLoop, 25);
 
-  // START MAINLOOP
-  var mainLoopId = setInterval(mainLoop, 25);
-});
