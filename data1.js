38e16eb963e94af1aedbdc7fa4f40d4b0dae0968
eternity18 <eternity18@hotmail.com>
2011-04-14 12:41:46 +0200

    Noelia

---
+++ data1.js
@@ -0,0 +1,33 @@
+var maxActivitats = 3;
+var tipusActivitat = [];
+var dadesActivitat = [];
+
+tipusActivitat[1] = "PuzzleDoble";
+dadesActivitat[1] = {
+					"textInit": "Activitat 1",
+					"textFinal": "FINALITZAT",
+					"imatge" : { "src" : './images/construccions.jpg', "lines": 3, "cols": 3},
+					"distribucio" : "AB"
+					};
+
+tipusActivitat[2] = "PuzzleIntercanvi";
+dadesActivitat[2] = {
+					"textInit": "Activitat 3",
+					"textFinal": "FINALITZAT",
+					"imatge" : { "src" : './images/pingui.jpg', "lines": 3, "cols": 3}
+					};
+
+tipusActivitat[3] = "Memoria";
+dadesActivitat[3] = {
+					"textInit": "Activitat 2",
+					"textFinal": "FINALITZAT",
+					"imatge" : { "src" : './images/pingui.jpg'},
+					"llistaImatges" : [{ "src" : './images/pirata2[1].jpg'},
+					                   { "src" : './images/pirata1[1].png'},
+					                   { "src" : './images/espada_pirata_63_cm_CM035LI_1[1].jpg'},
+					                   { "src" : './images/dibuix pirata.jpg'},
+					                   { "src" : './images/barret.jpg'},
+					                   { "src" : './images/GarfioPirata[1].jpg'}],
+					"cols": 6,
+					"lines": 1
+					};
---
+++ js/base_main.js
@@ -0,0 +1,69 @@
+function iniciaActivitat(canvas,num) {
+	var act;
+	//Posar aqui tots els tipus d'activitat que hi hagi
+	if (tipusActivitat[num] == "Memoria") { act = new Memoria(); }
+	if (tipusActivitat[num] == "PuzzleDoble") { act = new PuzzleDoble(); }
+	if (tipusActivitat[num] == "PuzzleIntercanvi") { act = new PuzzleIntercanvi(); }
+	
+	//Inicialitzar l'activitat
+	act.init(canvas, dadesActivitat[num]);
+	return act;
+}
+
+
+////////////////////////////////////////////////
+var canvas = document.getElementById('canvas');
+var canvasControl = document.getElementById('canvasControl');
+
+	
+//Inicialitzem l'interficie d'usuari
+var UI = new UserInterface();
+UI.init(canvasControl);
+var uiClic;
+
+//Triem la primera activitat i l'inicalitzem
+var numActivitat = 1;
+var activitatActual=iniciaActivitat(canvas, numActivitat);
+
+
+// MAIN LOOP FUNCTION
+var mainLoop = function () {	
+	
+	//Dibuixem l'interficie d'usuari
+	UI.draw();
+	
+	//Mirem si hi ha algun event a la interficie d'usuari
+	uiClic = UI.checkClics();
+	if (uiClic == "next")
+	{	
+		//Mirem si podem avan�ar a la segŸent activitat
+		if (numActivitat < maxActivitats) 
+		{
+			//Tanquem l'activitat anterior
+			activitatActual.end();
+			
+			//I comencem la nova
+			numActivitat++;
+			activitatActual=iniciaActivitat(canvas, numActivitat);
+		}
+	} 
+	else if (uiClic == "previous") {
+		//Mirem si podem anar a l'activitat anterior
+		if (numActivitat > 1) 
+		{
+			//Tanquem l'activitat anterior
+			activitatActual.end();
+			
+			//I comencem la nova
+			numActivitat--;
+			activitatActual=iniciaActivitat(canvas, numActivitat);
+		}
+	}
+	
+	//Despres actualitzem l'activitat que tenim en progres
+	activitatActual.run();
+
+};
+
+// START MAINLOOP
+var mainLoopId = setInterval(mainLoop, 25);
